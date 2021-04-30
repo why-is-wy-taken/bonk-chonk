299fedb33e6c476f687e985fd32d9222eb2c769e
wy <wy@dimension.sh>
2021-04-30 02:36:48 -0500

    Multiple: - Updated gitignore - Added debug menu

--- assets/scripts/Player.gd
+++ assets/scripts/Player.gd
@@ -2,21 +2,30 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
-const SPEED = 400
-const JUMP_HEIGHT = -450
+var speed = 400
 var motion = Vector2()
+
+# Debug shit
+var title = "Bonk Chonk WIP Pre-Alpha"
+func _on_SpeedUp_pressed():
+	speed += 50
+func _on_SpeedDown_pressed():
+	speed -= 50
+func _on_SpeedLabel_draw():
+	pass
+	
 
 func _physics_process(delta):
 	
 	if motion == Vector2(0, 0):
 		if Input.is_action_just_pressed("ui_right"):
-			motion.x = SPEED
+			motion.x = speed
 		elif Input.is_action_just_pressed("ui_left"):
-			motion.x = -SPEED
+			motion.x = -speed
 		elif Input.is_action_just_pressed("ui_up"):
-			motion.y = -SPEED
+			motion.y = -speed
 		elif Input.is_action_just_pressed("ui_down"):
-			motion.y = SPEED
+			motion.y = speed
 
 	if is_on_floor():
 		var _motion = Vector2(0, 0)
@@ -25,8 +34,8 @@ func _physics_process(delta):
 		print(motion)
 	
 	motion = move_and_slide(motion, UP)
-	pass
 
-	# Debug button to easily quit (end)
+	# Other debug shit
 	if Input.is_action_pressed("select"):
 		get_tree().quit()
+	OS.set_window_title(title + " | speed: " + str(speed) + " | FPS: " + str(Engine.get_frames_per_second()))
